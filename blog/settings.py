4f3fd956282d2778e7f6d9ffda3605d6d71c3e37
Joao Futi Muanda <52241312+codemesma@users.noreply.github.com>
2022-07-09 02:27:02 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -176,17 +176,15 @@ USE_TZ = True
 
 
 STATIC_ROOT= "/static/"
-
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (BASE_DIR,"static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+
 
 
 # Custom Django auth settings
