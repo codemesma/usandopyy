c2f0deb6051ca98c614025a3c9b79ef2bae92510
Joao Futi Muanda <52241312+codemesma@users.noreply.github.com>
2022-08-06 11:11:07 +0530

    filter

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,7 @@ from .models import Tutorial, Categoria, Tipo, Sobre, Author, Download
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'is_active')
+    list_display = ('name', 'is_active')
 
 
 class TipoAdmin(admin.ModelAdmin):
@@ -11,7 +11,18 @@ class TipoAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'user', 'category', 'status')
+    list_display = ('title', 'user', 'category', 'status')
+    
+    # Add filters for state and stars
+    list_filter = ['category', 'user']
+
+    # Make the Business list searchable by name
+    search_fields = ['title']
+
+    # We don't want ids showing up
+    exclude = ('id',)
+    
+
 
 
 admin.site.register(Categoria, CategoryAdmin)
